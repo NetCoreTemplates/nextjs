59730007871b730e738f6478bcea604dc9a6db9a
Darren Reid <darren@reidmail.org>
2022-06-09 16:52:40 +1000

    Remove CI debug

--- .deploy/ci.prebuild.sh
+++ .deploy/ci.prebuild.sh
@@ -1,3 +1,2 @@
 sed -i "s/\$DEPLOY_API/${deploy_api}/g" ./ui/next.config.js
 [[ -f ./ui/post.build.js ]] && sed -i "s/\$DEPLOY_API/${deploy_api}/g;s/\$DEPLOY_CDN/${deploy_cdn}/g" ./ui/post.build.js
-cat ./ui/next.config.js
