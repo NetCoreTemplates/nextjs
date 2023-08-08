b9e477ed650209da8b33ae212d5099859c348648
Darren Reid <darren@reidmail.org>
2023-08-08 14:02:58 +1000

    Update docker compose deploy related files.

--- .deploy/ci.prebuild.sh
+++ .deploy/ci.prebuild.sh
@@ -1,2 +1,2 @@
-sed -i "s/\$DEPLOY_API/${deploy_api}/g" ./ui/next.config.js
+sed -i "s/\$DEPLOY_API/${deploy_api}/g" ./ui/next.config.mjs
 [[ -f ./ui/post.build.js ]] && sed -i "s/\$DEPLOY_API/${deploy_api}/g;s/\$DEPLOY_CDN/${deploy_cdn}/g" ./ui/post.build.js
