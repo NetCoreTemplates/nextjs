f82e0cca2a19eb8fdfc706e5c4a90d1f9cd4d150
Demis Bellot <demis.bellot@gmail.com>
2025-11-11 16:05:42 +0800

    remove legacy deps

--- MyApp.Client/vitest.setup.ts
+++ MyApp.Client/vitest.setup.ts
@@ -1,9 +1,7 @@
-import { expect, afterEach } from 'vitest'
+import { afterEach } from 'vitest'
 import { cleanup } from '@testing-library/react'
-import * as matchers from '@testing-library/jest-dom/matchers'
 
-expect.extend(matchers)
-
+// Cleanup after each test (only needed for React component tests)
 afterEach(() => {
   cleanup()
 })
